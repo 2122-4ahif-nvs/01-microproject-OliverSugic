871ccecda0d90ad4ea1119f1c30eb1a34c390dac
oliversugic <osugic@gmail.com>
2022-01-04 21:12:41 +0100

    added Repository Test

---
+++ backend/src/test/java/at/htl/control/TrainerRepositoryTest.java
@@ -0,0 +1,37 @@
+package at.htl.control;
+
+import at.htl.enitity.Trainer;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class TrainerRepositoryTest {
+
+    @Inject
+    TrainerRepository trainerRepository;
+
+    @Test
+    void findAllSquads() {
+
+    }
+
+    @Test
+    void findByLastname() {
+        Trainer trainer = trainerRepository.findByLastname("Nachname");
+        assertThat(trainer.getLastname()).isEqualTo("Nachname");
+    }
+
+    @Test
+    void findAll(){
+        List<Trainer> persons = trainerRepository.findAll();
+        assertThat(persons.size()).isEqualTo(3);
+    }
+}
