1c6cd3ad81e73a8619c38adfd5cc722c3cc04683
oliversugic <osugic@gmail.com>
2022-01-04 21:10:59 +0100

    changed from insert to import

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+-- Trainer
+Insert into trainer(dob,firstname,lastname) values('01-01-1980','Trainer','Nachname');
+Insert into trainer(dob,firstname,lastname) values('01-01-1980','Trainer','Cooler Nachname');
+Insert into trainer(dob,firstname,lastname) values('01-01-1980','Trainer','Nicht cooler Nachname');
+
+
+-- Squads
+Insert into squad(ageforsquad,name,trainer_id) values('01-01-','Trainer',1);
+Insert into squad(ageforsquad,name,trainer_id) values('01-01-1980','Trainer','Nicht cooler Nachname');
+Insert into squad(ageforsquad,name,trainer_id) values('01-01-1980','Trainer','Nicht cooler Nachname');
+
