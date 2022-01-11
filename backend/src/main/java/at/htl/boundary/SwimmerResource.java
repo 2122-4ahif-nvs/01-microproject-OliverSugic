b50f177f23a335032eb260da0a97180906ef0caa
oliversugic <osugic@gmail.com>
2022-01-11 13:07:55 +0100

    Sugic - changes in the Resource clas

--- backend/src/main/java/at/htl/boundary/SwimmerResource.java
+++ backend/src/main/java/at/htl/boundary/SwimmerResource.java
@@ -2,17 +2,21 @@ package at.htl.boundary;
 
 import at.htl.control.SwimmerRepository;
 import at.htl.enitity.Swimmer;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.xml.transform.Templates;
 import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
 
 @Path("/swimmer")
 public class SwimmerResource {
@@ -20,19 +24,57 @@ public class SwimmerResource {
     @Inject
     SwimmerRepository repo;
 
-    @Path("/newswimmer")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response Swimmmer(@FormParam("firstname") String fname,
-                            @FormParam("lastname") String lname,
-                            @FormParam("bestStroke") String bestStroke,
-                            @Context UriInfo uriInfo
-    ){
+    /*
+        @CheckedTemplate
+        public static class Templates {
+            public static native TemplateInstance swimmer(Swimmer swimmer);
+        }
+    */
 
-        repo.insert(new Swimmer(fname, lname, null, bestStroke, 100, 100));
+    @CheckedTemplate(requireTypeSafeExpressions = false)
+    public static class Templates {
+        public static native TemplateInstance swimmer(List<Swimmer> swimmers);
+    }
+
+    @POST
+    @Path("/newswimmer")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response Swimmmer(@FormParam("firstname") String fname,
+                             @FormParam("lastname") String lname,
+                             @FormParam("bestStroke") String bestStroke,
+                             @FormParam("SwamKilometers") int kS,
+                             @FormParam("AttendedHours") int aH,
+                             @Context UriInfo uriInfo
+    ) {
+
+        repo.insert(new
+                Swimmer(
+                fname,
+                lname,
+                LocalDate.now(),
+                bestStroke,
+                kS,
+                aH)
+        );
 
         URI uri = uriInfo.getAbsolutePathBuilder().path("42").build();
-
         return Response.created(uri).build();
     }
+
+    /*
+        @GET
+        @Path("{id}")
+        @Produces(MediaType.TEXT_HTML)
+        public TemplateInstance get(@PathParam("id") Long id) {
+            return Templates.swimmer(repo.findSwimmer(id));
+        }
+    */
+    @GET
+    @Path("/getAllSwimmers")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance getAll() {
+        return Templates.swimmer(repo.findAll());
+    }
+
 }
